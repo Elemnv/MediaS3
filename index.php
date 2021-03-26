b440ba54c18228f773efc5ed931f081c18a8c0dd
Elmnva <elenamineeva02@mail.ru>
2021-03-26 19:13:11 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php ?>
+<?php require_once "form.php"?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<form action="/" method="post" class="flex-block">
+    <span>Имя</span>
+    <input type="text" name="name" value="">
+    <span>Фамилия</span>
+    <input type="text" name="surname">
+    <span>логин</span>
+    <input type="text" name="login">
+    <span>Пароль</span>
+    <input type="text" name="password">
+
+    <button type="submit">Отправить</button>
+</form>
+<?php $validate = valid($_POST); ?>
+
+<?php if (!empty($validate['error']) && $validate['error']){
+    foreach( $validate['messages'] as $key) {
+        echo '<p class="item-err">'. $key . '</p>';
+    }
+
+}
+
+?>
+
+<?php if (!empty($validate['success']) && $validate['success']){
+    foreach( $validate['messages'] as $key  ) {
+        echo  '<p class="item">'. $key . '</p>';
+    }
+}
+?>
+
+</body>
+</html>
