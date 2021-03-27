50a3b657bd50c4e142457ff9a6e1a27b3d904b1a
Elmnva <elenamineeva02@mail.ru>
2021-03-27 11:00:58 +0400

    Update index.php

--- index.php
+++ index.php
@@ -14,15 +14,15 @@
 
 <form action="/" method="post" class="flex-block">
     <span>Имя</span>
-    <input type="text" name="name" value="">
+    <input type="text" name="firstName" value="">
     <span>Фамилия</span>
-    <input type="text" name="surname">
-    <span>логин</span>
+    <input type="text" name="lastName">
+    <span>Логин</span>
     <input type="text" name="login">
     <span>Пароль</span>
     <input type="text" name="password">
 
-    <button type="submit">Отправить</button>
+    <button type="submit">ОТПРАВИТЬ</button>
 </form>
 <?php $validate = valid($_POST); ?>
 
